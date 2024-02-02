3dd2c9119797c504c3dbc7c760a864ed71ca3d39
asda-guidelines <97886059+asda-guidelines@users.noreply.github.com>
2024-02-02 14:18:41 +0000

    Delete JS directory

--- JS/notice.js
+++
@@ -1,8 +0,0 @@
-$("#NoticeClose").click(function() {
-    //set sessionStorage on click
-    sessionStorage.setItem("dismissNotice", "Hello");
-    $("#Notice").remove();
-});
-if (sessionStorage.getItem("dismissNotice"))
-    //When sessionStorage is set Do stuff...
-    $("#Notice").remove();
